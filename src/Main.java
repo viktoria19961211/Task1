bf7ad89daf8bffe63acfab34117fa8b08a92925e
viktoriia_kadzaeva <ms.kadzaeva@mail.ru>
2024-05-20 16:16:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Пожалуйста, введите нужное количество элементов:");
+        int numberOfElements = new Scanner(System.in).nextInt();
+        System.out.println("Пожалуйста, введите длину массива:");
+        int[] arrays = new int[new Scanner(System.in).nextInt()];
+        int temp = 1;
+        int firstNumber = temp;
+        boolean flagStop = true;
+        StringBuilder resault = new StringBuilder();
+
+        while (flagStop) {
+
+            for (int i = 0; i < arrays.length; i++) {
+
+                arrays[i] = temp;
+                ++temp;
+
+                if (temp > numberOfElements) {
+                    temp = 1;
+                }
+            }
+
+            temp = arrays[arrays.length - 1];
+            if (arrays[arrays.length - 1] == firstNumber) {
+                flagStop = false;
+            }
+            resault.append(arrays[0]);
+        }
+        System.out.println(resault);
+
+
+    }
+}
